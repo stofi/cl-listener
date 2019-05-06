3557aea8e4c20d29df6ae5df585cae55227e9df2
Kryštof Rainisch <krystof.rainisch@sprites.co>
2019-05-06 17:25:52 +0200

    add logging

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
+const package = require('./package')
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+const log = (...args) => {
+  console.log(`[${package.name}]`, ...args)
+}
+
 express()
-  .get('/', (req, res) => res.send(200))
+  .get('/', (req, res) => {
+    let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+    res.sendStatus(200)
+    log(`${new Date()}`)
+    log(`New connection from: ${ip}`)
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
