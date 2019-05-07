72f63ba0002891b0b8421702378b53701b2399eb
Kryštof Rainisch <krystof.rainisch@sprites.co>
2019-05-07 15:36:54 +0200

    change index

--- index.js
+++ index.js
@@ -3,17 +3,34 @@ const bodyParser = require('body-parser')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const { log } = require('./app/lib/utils')
+const getBudget = require('./app/cl-api')
 
 const app = express()
 
 app.use(bodyParser.json())
 
-app.all('/', (req, res) => {
+app.all('/', async (req, res) => {
     res.sendStatus(200)
     let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
     log(`${new Date()}`)
     log(`New connection from: ${ip}`)
-    let data = req.body.data.map(entry => entry.data )
-    log('\n', data)
+    if(!req.body.data) return
+
+    log(`Parsing data`)
+    let data = req.body.data.map(event=>event.data[0])
+    let ids = []
+    data.forEach(entry=>{
+      let pid = entry.assignment.project_id
+      if (ids.indexOf(pid) === -1) ids.push(pid)
+    })
+    log('Data:\n')
+    log(data)
+    log('IDs:\n')
+    log(ids)
+    log('Budgets:\n')
+    for (id of ids) {
+      let budget = await getBudget(id)
+      log(id, budget)
+    }
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
