4b703a9540b0f5bc2fc53b6400f0156916f12ac8
Kryštof Rainisch <krystof.rainisch@sprites.co>
2019-05-07 11:26:11 +0200

    add body parser

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const { log } = require('./app/lib/utils')
 
+const app = express()
 
-express()
-  .all('/', (req, res) => {
+app.use(bodyParser.json({
+  type: 'application/*+json'
+}))
+
+app.all('/', (req, res) => {
     res.sendStatus(200)
     let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
     log(`${new Date()}`)
     log(`New connection from: ${ip}`)
+    log('\n', req.body)
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
