f5ba9feb7cdbb62413ed708a3ac23b07748b9689
Kryštof Rainisch <krystof.rainisch@sprites.co>
2019-05-08 14:50:52 +0200

    add test

---
+++ tests/addproject.js
@@ -0,0 +1,10 @@
+const updateProject = require('../app/update-project')
+updateProject({
+  id: 420,
+  name: 'test-project',
+  activity: 'test-activity',
+  person: 'test-person',
+  task: 'test-task',
+  allocated: 16,
+  spent: 16.3333333333333
+})
