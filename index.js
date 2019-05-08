36871e20c10c4646182069a00e065530d9e593ca
Kryštof Rainisch <krystof.rainisch@sprites.co>
2019-05-08 13:25:55 +0200

    change project processing

--- index.js
+++ index.js
@@ -18,21 +18,45 @@ app.all('/', async (req, res) => {
 
     log(`Parsing data`)
     let data = req.body.data.map(event=>event.data[0])
+
     let ids = []
-    data.forEach(entry=>{
-      let pid = entry.assignment.project_id
+    data = data.filter(event=>{
+      let pid = event.assignment.project_id
+      if (pid !== null && ids.indexOf(pid) === -1) {
+        ids.push(pid)
+        return true
+      } else return false
+    })
+    let projects = data.map(event=>({
+
+    }))
+    let projects = data.map(event=> ({
+      id: event.assignment.project_id,
+      name: event.names.project_name,
+      activity: event.names.activity_name,
+      person: event.names.person_fullname,
+      task: event.names.task_name || ''
+    }))
+
+    data.forEach(event=>{
+      let pid = event.assignment.project_id
       if (pid !== null && ids.indexOf(pid) === -1) ids.push(pid)
     })
     if(ids.length === 0) return
 
     log('Data:')
     log(data)
+    log('Projects:')
+    log(projects)
     log('IDs:')
     log(ids)
     log('Budgets:')
-    for (id of ids) {
+    for (project of projects) {
+      let id = project.id
       let budget = await getBudget({id})
-      log(id, budget)
+      project.allocated = budget.allocated
+      project.spent = budget.spent
+      log(project)
       // dont just log this
       // now it's time to use the database
     }
