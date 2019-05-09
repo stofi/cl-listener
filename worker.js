53e42fb0d5fccfc2bd3be414e8e86a41632d608a
Kryštof Rainisch <krystof.rainisch@sprites.co>
2019-05-09 10:33:51 +0200

    add worker

---
+++ worker.js
@@ -0,0 +1,38 @@
+const apiKey = process.env.MAILGUN_API_KEY
+const domain = process.env.MAILGUN_DOMAIN
+const CLIENT_EMAIL_ADDRESS = process.env.CLIENT_EMAIL_ADDRESS
+
+const mailgun = require('mailgun-js')({apiKey, domain})
+const mongoose = require('mongoose');
+const controller = require('./app/controller')
+const { log } = require('./app/lib/utils')
+const mongoURI = process.env.MONGO_URI || "mongodb://localhost"
+
+mongoose
+  .connect(mongoURI, {
+    useNewUrlParser: true,
+    dbName: 'cl-test-db'
+  })
+  .then(async mg => {
+    let projects = await controller.index()
+
+    projects = projects.filter(p=>((p.allocated < p.spent) && !p.didNotify))
+
+    for (project of projects) {
+      let data = {
+        to: CLIENT_EMAIL_ADDRESS,
+        subject: `Exceeded allocated time on ${project.name}`,
+        text: `
+          Project: ${project.name}
+          Activity: ${project.activity}
+          Person: ${project.person}
+          Task: ${project.task}
+          Time: ${(project.allocated).toFixed(2)}/${(project.spent).toFixed(2)}
+        `
+      }
+      await mailgun.messages().send(data)
+    }
+
+    log('Closed the database connection')
+  })
+  .catch(log)
